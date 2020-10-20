6e05d96e7601253ef3a98256ab9ae0173884dab7
sifat007 <sifat27.ht@gmail.com>
2020-10-20 12:10:57 +0600

    done app

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,183 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CountDownTimer(),
+    )
+);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class CountDownTimer extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _CountDownTimerState createState() => _CountDownTimerState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _CountDownTimerState extends State<CountDownTimer> {
+  double percent = 0;
+  static int TimeInMinut = 25;
+  int TimeInSec = TimeInMinut * 60;
+  Timer timer;
+  void _StartTimer(){
+    TimeInMinut = 25;
+    int Time = TimeInSec;
+    double SecPercent = (Time/100) ;
+    print(SecPercent);
+    timer = Timer.periodic(Duration(seconds: 1), (timer){
+      setState(() {
+        if(Time >0){
+          Time--;
+          if(Time % 60 == 0){
+            TimeInMinut--;
+          }
+          if(Time% SecPercent == 0){
+            if(percent <1){
+              percent+=0.01;
+            }else{
+              percent = 1;
+            }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+          }
+        }else{
+          percent = 0;
+          TimeInMinut = 25;
+          timer.cancel();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+        }
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xFF1542BF),Color(0xFF51A8FF)],
+                  begin: FractionalOffset(0.5,1)
+              )
+          ),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top:18.0),
+                child: Text(
+                  "Pomodoro",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50.0
+                  ),
+                ),
+              ),
+              Expanded(
+                child: CircularPercentIndicator(
+                  percent: percent,
+                  animation: true,
+                  lineWidth: 20.0,
+                  circularStrokeCap: CircularStrokeCap.round,
+                  reverse: false,
+                  animateFromLastPercent: true,
+                  radius: 250.0,
+                  progressColor: Colors.white,
+                  center: Text(
+                    "$TimeInMinut",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 80.0,
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 30.0,),
+              Expanded(
+                child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(topLeft:Radius.circular(30.0),topRight: Radius.circular(30.0))
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child:  Column(
+                                    children: <Widget>[
+                                      Text(
+                                        "Study Time",
+                                        style: TextStyle(
+                                          fontSize: 30.0,
+                                        ),
+                                      ),
+                                      SizedBox(height: 10.0,),
+                                      Text(
+                                        "25",
+                                        style: TextStyle(
+                                            fontSize: 40.0
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child:  Column(
+                                    children: <Widget>[
+                                      Text(
+                                        "Break Time",
+                                        style: TextStyle(
+                                          fontSize: 30.0,
+                                        ),
+                                      ),
+                                      SizedBox(height: 10.0,),
+                                      Text(
+                                        "5",
+                                        style: TextStyle(
+                                            fontSize: 40.0
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 28.0),
+                            child: RaisedButton(
+                              color: Colors.blue,
+                              shape:RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100.0),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Text("Start Studying", style: TextStyle(color:Colors.white,fontSize: 22.0),),
+                              ),
+                              onPressed: _StartTimer,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                ),
+              )
+
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
